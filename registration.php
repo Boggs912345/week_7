66c08aa9575520816cce58017c7f6a8ce49513c0
30115551 <30115551@students.southwales.ac.uk>
2025-02-27 12:40:02 +0000

    test

--- registration.php
+++ registration.php
@@ -6,7 +6,7 @@
     <title>Bootstrap demo</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+<h1>Register for services</h1>
 </body>
 
 
@@ -20,6 +20,10 @@
         <label for="exampleInputPassword1">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password confirmation</label>
+        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+    </div>
     <div class="form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
@@ -27,4 +31,3 @@
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </html>
-
